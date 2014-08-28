1759449c8afdaeb03c460f69a2bbb3f9d83bb583
Thiago Couto <coutocodes@gmail.com>
2014-08-28 00:16:08 -0400

    More scripting

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-	console.log('a user connected');
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
 
 http.listen(3000, function(){
