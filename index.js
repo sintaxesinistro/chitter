89d0678a9b373fdbb84fe303da281d0c5ea19509
Thiago Couto <coutocodes@gmail.com>
2014-08-28 00:41:24 -0400

    Chitter chatter

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ io.on('connection', function(socket){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+  	io.emit('chat message', msg);
     console.log('message: ' + msg);
   });
 });
